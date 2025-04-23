c331963a4283b7c8c413045475a18aa97b5f9abd
Gazi2050 <gazi45344@gmail.com>
2025-04-23 22:23:10 +0600

    improve:improved code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,29 +1,7 @@
+import { heroData } from '@/lib/Data';
 import Link from 'next/link';
 import React from 'react';
 
-const heroData = {
-    title: "Filo",
-    description: "Your minimalist notebook — stay organized, focused, and productive.",
-    stepsTitle: "How to Get Started",
-    steps: [
-        <>
-            Click <code className="bg-zinc-700/70 px-1 py-0.5 rounded text-sm">Get Start</code> to start typing — your entire page is your canvas.
-        </>,
-        <>
-            Use <code className="bg-zinc-700/70 px-1 py-0.5 rounded text-sm">/</code> to quickly access commands like headings, paragraph, or table.
-        </>,
-        <>
-            <strong>Auto-save</strong> ensures your work is always saved without lifting a finger.
-        </>,
-        <>
-            Organize content easily with <strong>drag-and-drop blocks</strong>, similar to Notion.
-        </>,
-        <>
-            Enjoy a <strong>minimalist, faster, and private</strong> alternative to other platforms like <a className='text-blue-500 underline underline-offset-4 font-medium' href="https://blank.page" target='_blank'>Black.page</a>
-        </>
-    ],
-    buttonText: "Get Start"
-};
 const Hero = () => {
     return (
         <section className="text-center mt-10 px-4" aria-labelledby="hero-heading">
